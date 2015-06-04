f999d3b30d42e5b66d0100363e8b8c560857a12a
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-06-03 23:11:13 -0400

    Add graph construction
    Still lack the ability to invoke proper operations on the built graph.

--- apps/myapp/myapp.cpp
+++ apps/myapp/myapp.cpp
@@ -1,69 +1,86 @@
 #include <graphlab.hpp>
 #include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <utility> //for pair
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::pair;
+using std::string;
+using std::cout;
+using std::cin;
+using namespace graphlab;
+
+//This should be modified by the commandline option.
+static const int K = 4;
 
 //qqiu: inherit from POD do not require save and load to serialize
-typedef struct node : public graphlab::IS_POD_TYPE {
+struct collection_gather: public graphlab::IS_POD_TYPE {
     double x, y, z, mass;
-} vertex_data_type;
+    vector< double > KNN;
 
-class knn{
-    public:
-        edge_dir_type gather_edges(icontext_type& context, 
-                const vertex_type& vertex) const{
-            return graphlab::ALL_EDGES;
-        }
-        gather_type gather(){
-        }
-};
+    collection_gather(){
 
-//how do you set this to gather type
-//nothing special just return directly
-//This is the object that is being passed by gather to apply
-struct dist_vec{
-    vector<float> dists; 
-};
-
-class setDistance{
-    public:
-    //for each connected point, calculate the distance and
-    gather_type gather(icontext_type& context,
-            const vertex_type& vertex) const{
-        dist_vec vec;
-        float cnt_dist = calculate_dist(edge.source(), vertex);
-        vec.dists.push_back(cnt_dist);
-        return dists;
     }
 
-    apply(icontext_type& context, vertex_type& vertex, 
-            const gather_type &allneighbors){
-        size_t allneighbors.dists.size();
-        for all in-edges:
-            dists.insert(cnt_dist);
-        return success; 
+    collection_gather& operator+=(const collection_gather &other) {
+        return *this;
     }
-
 };
+
+
+
 
 typedef graphlab::empty edge_data_type;
 
-typedef graphlab::distributed_graph<vertex_data_type, 
+typedef graphlab::distributed_graph<collection_gather, 
         edge_data_type> graph_type;
 
+class setDistance : public graphlab::ivertex_program<graph_type, vector<double>>{
+    //double computeDist(const vertex_data_type& x, const vertex_data_type& y){
+    //    return sqrt( pow(x.x-y.x, 2) + pow(x.y-y.y, 2) + pow(x.z-y.z, 2));
+    //}
+    public:
+        //gather only inedges
+        edge_dir_type gather_edges(icontext_type &context, 
+                const vertex_type& vertex) const {
+            return graphlab::IN_EDGES; 
+        }
+        //for each connected point, calculate the distance and
+        gather_type gather(icontext_type &context, const vertex_type &vertex, const edge_type &edge) {
+            vector<double> v;
+            return v;
+        }
+
+        void apply(icontext_type& context, vertex_type& vertex, const gather_type &allneighbors){
+            return;
+        }
+
+};
+
+
+
 //input file will need to specify number of points.
-int main(int argc, char** argv) {
-    //read in the graph somewhere here
+int main(int argc, char** argv) 
+{
     graphlab::mpi_tools::init(argc, argv);
     graphlab::distributed_control dc;
 
-    graph_type graph;
-    graphlab::distributed_control dc(graphtype, cml_opt);
-    graphlab::omni_engine<setEdgeValue> setDistance(dc, graph, exec_type, clopts);
+    graphlab::command_line_options clopts("KNN algorithm");
 
-    //load graph;
-    //graph.finalize();
 
-    setDistance.signal_all(); //This marks all points as "active"
-    setDistance.start();
+    //have to have a dc for some graph
+    graph_type graph(dc);
+    //graphlab::omni_engine<setDistance>engineSetDistance(dc, graph, "sync", clopts); 
+    string path = "/u/qqiu/PowerGraph/release/apps/myapp/input/full";
+    string format = "adj";
+    graph.load_format(path,format);
+    graph.finalize();
+
+    //engineSetDistance.signal_all(); //This marks all points as "active"
+    //engineSetDistance.start();
 
 
     graphlab::mpi_tools::finalize();
