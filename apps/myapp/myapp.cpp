57199e3d7cec5ac7ee15ae598179099cadb5a851
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-06-04 00:38:56 -0400

    Add empty gather, apply, and graph engine
    Can only use async_consistent_engine but not omni_engine.
    What would be the reason?

--- apps/myapp/myapp.cpp
+++ apps/myapp/myapp.cpp
@@ -11,54 +11,65 @@ using std::pair;
 using std::string;
 using std::cout;
 using std::cin;
+using std::endl;
 using namespace graphlab;
 
-//This should be modified by the commandline option.
-static const int K = 4;
 
-//qqiu: inherit from POD do not require save and load to serialize
-struct collection_gather: public graphlab::IS_POD_TYPE {
-    double x, y, z, mass;
-    vector< double > KNN;
+typedef graphlab::vertex_id_type color_type;
 
-    collection_gather(){
+struct set_union_gather{
+    vector<color_type> colors;
 
-    }
-
-    collection_gather& operator+=(const collection_gather &other) {
+    set_union_gather& operator+=(const set_union_gather& other){
+        for(graphlab::vertex_id_type othervid = 0; 
+                othervid < other.colors.size(); othervid++){
+            colors.push_back(othervid);
+        }
         return *this;
     }
+
+    //serialize to disk 
+    void save(graphlab::oarchive& oarc) const{
+        oarc << colors; 
+    }
+
+    //unserilize from disk to RAM
+    void load(graphlab::iarchive& iarc) {
+        iarc >> colors;
+    }
 };
-
-
-
 
 typedef graphlab::empty edge_data_type;
+typedef graphlab::distributed_graph<color_type, edge_data_type> graph_type;
 
-typedef graphlab::distributed_graph<collection_gather, 
-        edge_data_type> graph_type;
+class setDistance: public graphlab::ivertex_program<graph_type, 
+    set_union_gather>, 
+    public graphlab::IS_POD_TYPE{
+        public:
 
-class setDistance : public graphlab::ivertex_program<graph_type, vector<double>>{
-    //double computeDist(const vertex_data_type& x, const vertex_data_type& y){
-    //    return sqrt( pow(x.x-y.x, 2) + pow(x.y-y.y, 2) + pow(x.z-y.z, 2));
-    //}
-    public:
-        //gather only inedges
-        edge_dir_type gather_edges(icontext_type &context, 
-                const vertex_type& vertex) const {
-            return graphlab::IN_EDGES; 
-        }
-        //for each connected point, calculate the distance and
-        gather_type gather(icontext_type &context, const vertex_type &vertex, const edge_type &edge) {
-            vector<double> v;
-            return v;
-        }
+            edge_dir_type gather_edges(icontext_type &context, 
+                    vertex_type vertex) const{
+                return graphlab::IN_EDGES;
+            }
 
-        void apply(icontext_type& context, vertex_type& vertex, const gather_type &allneighbors){
-            return;
-        }
+            //this happens for each edge 
+            //probably in order then ?
+            gather_type gather(icontext_type &context, 
+                    vertex_type &vertex,
+                    edge_type &edge){
+                set_union_gather gather;
+                //cout << edge.source().id()   <<endl;
+                //cout << edge.source().data() <<endl;
+                //cout << edge.target().id()   <<endl; 
+                //cout << edge.target().data() <<endl;
+                return gather;
+            }
 
-};
+            void apply(icontext_type &context, vertex_type &vertex, 
+                    const gather_type &neighbor){
+            
+            }
+    };
 
 
 
@@ -74,6 +85,7 @@ int main(int argc, char** argv)
     //have to have a dc for some graph
     graph_type graph(dc);
     //graphlab::omni_engine<setDistance>engineSetDistance(dc, graph, "sync", clopts); 
+    graphlab::async_consistent_engine<setDistance>engineSetDistance(dc, graph, clopts); 
     string path = "/u/qqiu/PowerGraph/release/apps/myapp/input/full";
     string format = "adj";
     graph.load_format(path,format);
