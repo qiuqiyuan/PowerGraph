c3da9139fad528663f476205564e4e5120204fa9
Jingwei Xu <fxkxjw@gmail.com>
2015-10-20 20:19:27 -0400

    test

--- release/apps/myapp/test.sh
+++ release/apps/myapp/test.sh
@@ -1,6 +1,6 @@
-for i in {1..24};
+for i in {1..16};
 	do echo $i >> log;
-	mpirun -np $i ./myapp --K 3 --knn_d 3 --input ./input/10k10k.txt --num_s 10000 >> log;
+	mpirun -np $i ./myapp --K 3 --ncpus $i --knn_d 3 --input ./input/1k10k.txt --num_s 1000 >> log;
 	echo "==========" >> log;
 	echo " " >> log;
 done
