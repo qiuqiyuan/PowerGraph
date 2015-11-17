df09998a786949ea19a136dc9f8356cbed07e6e3
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-11-16 20:53:36 -0500

    Convert to linux format

--- release/apps/myapp/run.sh
+++
@@ -1,1 +0,0 @@
-./myapp --input ./input/21.txt --K 3 --knn_d 3 --num_s 15
--- release/apps/myapp/test.sh
+++
@@ -1,8 +0,0 @@
-for i in {1..16};
-	do echo $i >> log;
-	mpirun -np $i ./myapp --K 3 --ncpus $i --knn_d 3 --input ./input/1k10k.txt --num_s 1000 >> log;
-	echo "==========" >> log;
-	echo " " >> log;
-done
-
-
